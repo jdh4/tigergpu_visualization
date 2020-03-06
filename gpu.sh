8fc693dbd0c7231a238b57cbbc19fa6be45a7ee6
jdh4 <jdh4@princeton.edu>
2020-03-06 09:03:34 -0500

    Removed zero padding in hour

--- gpu.sh
+++ gpu.sh
@@ -3,7 +3,7 @@ cd /scratch/gpfs/jdh4/gpustat
 /usr/licensed/anaconda3/2019.10/bin/python tigergpu_usage.py
 cp tigergpu_utilization.png /tigress/jdh4/public_html
 
-HOUR=$(date +%H)
+HOUR=$(date +%-H)
 MINUTES=$(date +%M)
 REMAIN=$(($HOUR % 2))
 if [[ $REMAIN -eq 0 && $MINUTES -lt 10 ]]; then
