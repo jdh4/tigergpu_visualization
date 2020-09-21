fee180ab33c7bd26d7d9da4964888414134828e9
jdh4 <jdh4@princeton.edu>
2020-09-20 23:56:26 -0400

    Only get info of previous users

--- make_cache.py
+++ make_cache.py
@@ -1,4 +1,4 @@
-#!/usr/licensed/anaconda3/2020.2/bin/python
+#!/usr/licensed/anaconda3/2020.7/bin/python
 import sys
 sys.path = list(filter(lambda p: p.startswith("/usr"), sys.path))
 sys.path.append('/scratch/gpfs/jdh4/gpustat')
@@ -6,11 +6,15 @@ import subprocess
 import pandas as pd
 from dossier import ldap_plus
 
-cmd = "getent passwd | cut -d: -f1 | sort | uniq"
-output = subprocess.run(cmd, capture_output=True, shell=True)
-netids = output.stdout.decode("utf-8").split('\n')
-netids.remove('')
-netids.remove('+')
+if 1:
+  df = pd.read_csv("utilization.csv", header=None)
+  netids = list(df.iloc[:,3].drop_duplicates().values)
+else:
+  cmd = "getent passwd | cut -d: -f1 | sort | uniq"
+  output = subprocess.run(cmd, capture_output=True, shell=True)
+  netids = output.stdout.decode("utf-8").split('\n')
+  netids.remove('')
+  netids.remove('+')
 
 univ_info = ldap_plus(netids)
 df = pd.DataFrame(univ_info[1:], columns=univ_info[0])
