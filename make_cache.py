37c55b20c7ad59ce771b8462aaf5511dd4b4c5b6
jdh4 <jdh4@princeton.edu>
2020-11-09 23:38:16 -0500

    Replaced Path.exists with is_file

--- make_cache.py
+++ make_cache.py
@@ -1,14 +1,16 @@
 #!/usr/licensed/anaconda3/2020.7/bin/python
 
+base = "/scratch/gpfs/jdh4/gpustat"
+
 import sys
 sys.path = list(filter(lambda p: p.startswith("/usr"), sys.path))
-sys.path.append('/scratch/gpfs/jdh4/gpustat')
+sys.path.append(base)
 import subprocess
 import pandas as pd
 from dossier import ldap_plus
 
 if 1:
-  df = pd.read_csv("utilization.csv", header=None)
+  df = pd.read_csv(f"{base}/utilization.csv", header=None)
   netids = list(df.iloc[:,3].drop_duplicates().values)
 else:
   cmd = "getent passwd | cut -d: -f1 | sort | uniq"
@@ -22,4 +24,4 @@ df = pd.DataFrame(univ_info[1:], columns=univ_info[0])
 cols = ['NETID', 'POSITION', 'DEPT', 'NAME', 'SPONSOR']
 df = df[cols]
 df = df[pd.notna(df.POSITION) | pd.notna(df.DEPT) | pd.notna(df.SPONSOR)]
-df.to_csv('/scratch/gpfs/jdh4/gpustat/cached_users.csv', columns=cols, index=False)
+df.to_csv(f"{base}/cached_users.csv", columns=cols, index=False)
