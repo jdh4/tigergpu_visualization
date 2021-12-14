ff4f63b033421e99ff78399e7fa11ea9f68a410f
jdh4 <52128661+jdh4@users.noreply.github.com>
2021-12-14 15:50:21 -0500

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
-ssh della-gpu "/home/jdh4/bin/gpus/a100.sh"
-ssh traverse "/home/jdh4/bin/gpus/v100.sh"
-/home/jdh4/bin/gpus/p100.sh
+timeout 30 /home/jdh4/bin/gpus/p100.sh
+timeout 30 ssh della-gpu "/home/jdh4/bin/gpus/a100.sh"
+timeout 30 ssh traverse "/home/jdh4/bin/gpus/v100.sh"
